8ae7fc95fd3cd3477628cedaa154998acc1b981e
sohamsshah <sohamshah456@gmail.com>
2020-12-18 17:57:13 +0530

    resolve module.css issue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/header'
 
 
 
 function App() {
   return (
     <div className="App">
-      <div class="header">
-      <h1> Parallelo<span class="heading-design">mojipedia</span></h1>
-      <input class="input" placeholder="Enter your emoji here"></input>
-      <button class="search-form__button" aria-label="search">🔍</button>
-      </div>
+    <Header/>
     </div>
 
   );
---
+++ src/Components/Header/header.js
@@ -0,0 +1,10 @@
+import'./header.css'
+export default function Header(){
+return (<div class="header">
+      <h1> Parallelo<span class="heading-design">mojipedia</span></h1>
+      <input class="input" placeholder="Enter your emoji here"></input>
+      <button class="search-form__button" aria-label="search">🔍</button>
+    </div>
+    );
+}
+
