ae8919790719f972ad960fbe3c4b2785ce7c1f41
sohamsshah <sohamshah456@gmail.com>
2020-12-18 11:36:24 +0530

    add input section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <div class="header">
       <h1> Parallelo<span class="heading-design">mojipedia</span></h1>
       <input class="input" placeholder="Enter your emoji here"></input>
+      <button class="search-form__button" aria-label="search">🔍</button>
       </div>
     </div>
-    
+
   );
 }
 
