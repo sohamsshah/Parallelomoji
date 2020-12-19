eee610502aece19e08426b2192afd0af0357265e
sohamsshah <sohamshah456@gmail.com>
2020-12-19 10:29:06 +0530

    update file structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
-import Header from './Components/Header/header'
 
 
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑":
+    "annoyance"
+};
 
 function App() {
-  return (
-    <div className="App">
-    <Header/>
-    </div>
+  const [emoji, setEmoji] = useState(""); 
+    const [meaning, setMeaning] = useState("translation will appear here..");
 
-  );
+    function changeHandler(event) {
+        const inputEmoji = event.target.value;
+        setEmoji(inputEmoji);
+    
+        if (inputEmoji in emojiDictionary) {
+          setMeaning(emojiDictionary[inputEmoji]);
+        } else {
+          setMeaning("failure to recognise this emoji");
+        }
+      }
+return (
+<div className="App">
+<div className="header">
+      <h1> Parallelo<span className="heading-design">mojipedia</span></h1>
+      <input onChange={changeHandler} className="input" placeholder="Enter your emoji here"></input>
+      <button className="search-form__button" aria-label="search">🔍</button>
+      <h2> {emoji} </h2> 
+      <h3> {meaning} </h3>
+    </div>
+    </div>)
 }
 
 export default App;
