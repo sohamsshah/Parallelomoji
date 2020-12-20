767a235a58dea03a1e5ab19001478f678e100d3a
sohamsshah <sohamshah456@gmail.com>
2020-12-20 16:45:17 +0530

    complete navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,33 @@ export default function Navbar(){
     return(
         <div>
               <nav className="navigation container">
-              <div className="nav-brand">  Parallelomojipedia </div>
+              <div className="nav-brand">  📙 Parallelo<span className="navbar-heading-design">mojipedia</span> </div>
               <ul class = "list-non-bullet nav-pills">
                 <li class="list-item-inline">
-                    <a class="link link-active" href="https://en.wikipedia.org/wiki/Morse_code">Smileys | </a>
+                    <a class="link link-active" href="https://emojipedia.org/people/">😃 Smileys and People | </a>
                 </li>
                 <li class="list-item-inline">
-                    <a class="link link-active" href="https://github.com/sohamsshah/Morse-decipher">Animals</a>
+                    <a class="link link-active" href="https://emojipedia.org/nature/">🐻 Animals and Nature |</a>
                 </li>
+                <li class="list-item-inline">
+                    <a class="link link-active" href="https://emojipedia.org/food-drink/">🍔 Food and Drink |</a>
+                </li>
+                <li class="list-item-inline">
+                    <a class="link link-active" href="https://emojipedia.org/activity/">⚽ Activity |</a>
+                </li>
+                <li class="list-item-inline">
+                    <a class="link link-active" href="https://emojipedia.org/travel-places/">🌇 Travel and Places |</a>
+                </li>
+                <li class="list-item-inline">
+                    <a class="link link-active" href="https://emojipedia.org/objects/">💡 Objects |</a>
+                </li>
+                <li class="list-item-inline">
+                    <a class="link link-active" href="https://emojipedia.org/symbols/">🔣 Symbols |</a>
+                </li>
+                <li class="list-item-inline">
+                    <a class="link link-active" href="https://emojipedia.org/flags/">🎌 Flags </a>
+                </li>
+                
             </ul>
             
         </nav>
